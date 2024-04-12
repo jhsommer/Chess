c656b8cf86ae8f79983393b37b62d164a4820114
JoshiStud <166837953+J0shuaLutz@users.noreply.github.com>
2024-04-12 17:47:34 +0200

    wir haben ein Fenster

--- Chess/Chess/Chess.cpp
+++ Chess/Chess/Chess.cpp
@@ -1,17 +1,48 @@
 #include <iostream>
 #include "SDL.h"
 using namespace std;
+
+#define TIC_INTERVAL 16
+
 int main(int argc, char* argv[])
 {
 	
-	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	if (SDL_Init(SDL_INIT_VIDEO) == 0)
 	{
+		SDL_Window* window = NULL;
+		SDL_Renderer* renderer = NULL;
+
+		if (SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer) == 0) {
+			SDL_bool done = SDL_FALSE;
+
+			while (!done) {
+				SDL_Event event;
+
+				SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+				SDL_RenderClear(renderer);
+				SDL_RenderPresent(renderer);
+
+				while (SDL_PollEvent(&event)) {
+					if (event.type == SDL_QUIT) {
+						done = SDL_TRUE;
+					}
+				}
+			}
+			if (renderer) {
+				SDL_DestroyRenderer(renderer);
+			}
+			if (window) {
+				SDL_DestroyWindow(window);
+			}
+		}
+
 	cout << "SDL initialization failed. SDL Error: " << SDL_GetError();
 	}
 	else
 	{
 	cout << "SDL initialization succeeded!";
 	}
+	SDL_Quit();
 	cin.get();
 	return 0;
 	
