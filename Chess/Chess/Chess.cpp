494255d9be4812b79c9c5f6a9186fca02791c3f5
jhsommer <129003263+jhsommer@users.noreply.github.com>
2024-04-12 17:16:03 +0200

    sdl

--- Chess/Chess/Chess.cpp
+++ Chess/Chess/Chess.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main(int argc, char* argv[])
 {
-	/*
+	
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
 	{
 	cout << "SDL initialization failed. SDL Error: " << SDL_GetError();
@@ -14,6 +14,6 @@ int main(int argc, char* argv[])
 	}
 	cin.get();
 	return 0;
-	*/
+	
 }
 
